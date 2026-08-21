917255ad63a1dbed823ef0513e5b4c7184799625
firefingers21 <online+github@alexhuisman.ca>
2026-08-20 21:25:48 -0700

    Shorten timeouts for downloading data

--- Workflow/reload.sh
+++ Workflow/reload.sh
@@ -11,12 +11,12 @@ function getSeason {
     seasonDir="${alfred_workflow_data}/${seasonYear}"
 }
 [[ -f "${seasons_file}" ]] && getSeason
-[[ -n "${season}" ]] && downloadStatus=1 || curl -sf --compressed --connect-timeout 10 -L "https://stats-api.mlssoccer.com/competitions/MLS-COM-000001/seasons" -o "${seasons_file}" && downloadStatus=1 && getSeason
+[[ -n "${season}" ]] && downloadStatus=1 || curl -sf --compressed --connect-timeout 5 -L "https://stats-api.mlssoccer.com/competitions/MLS-COM-000001/seasons" -o "${seasons_file}" && downloadStatus=1 && getSeason
 
 if [[ -n "${downloadStatus}" ]]; then
     # Get season standings
     mkdir -p "${seasonDir}"
-    curl -sf --compressed --connect-timeout 10 --parallel \
+    curl -sf --compressed --parallel --max-time 10 \
         -L "https://stats-api.mlssoccer.com/competitions/MLS-COM-000001/seasons/${season}/standings?category=conference" -o "${seasonDir}/conferenceStandings.json" \
         -L "https://stats-api.mlssoccer.com/competitions/MLS-COM-000001/seasons/${season}/standings" -o "${seasonDir}/leagueStandings.json" \
         -L "https://stats-api.mlssoccer.com/statistics/clubs/competitions/MLS-COM-000001/seasons/${season}?per_page=50" -o "${seasonDir}/stats.json"
@@ -25,7 +25,7 @@ if [[ -n "${downloadStatus}" ]]; then
         # Get Team Logos
         mkdir -p "${seasonDir}/icons"
         teamLogos="$(jq -r '[.tables[].entries[].team_three_letter_code] | join(",")' "${seasonDir}/leagueStandings.json")"
-        curl -sf --compressed --parallel --output-dir "${seasonDir}/icons" -L "https://images.mlssoccer.com/image/upload/assets/logos/{${teamLogos}}.svg" -o "#1.svg"
+        curl -sf --compressed --parallel --max-time 10 --output-dir "${seasonDir}/icons" -L "https://images.mlssoccer.com/image/upload/assets/logos/{${teamLogos}}.svg" -o "#1.svg"
     fi
     touch "${alfred_workflow_data}"
     printf "Standings Updated"
