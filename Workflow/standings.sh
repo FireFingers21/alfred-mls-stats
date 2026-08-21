2d3107cda0cf99465ad6c77c20701b1c91ec7475
firefingers21 <online+github@alexhuisman.ca>
2026-08-20 21:56:01 -0700

    Add shortcut to set/unset favourite teams

--- Workflow/standings.sh
+++ Workflow/standings.sh
@@ -11,30 +11,34 @@ set -o extendedglob
 
 # Load Standings
 jq -cs \
+   --arg alfred_workflow_keyword "${alfred_workflow_keyword}" \
    --arg favTeam "$(iconv -f UTF-8-MAC -t UTF-8 <<< ${(L)favTeam})" \
    --arg grouping "${grouping}" \
    --arg icons_dir "${seasonDir}/icons" \
    --arg seasonYear "${seasonYear}" \
 '{
     "variables": {
+        "keyword": $alfred_workflow_keyword,
         "icons_dir": $icons_dir,
         "seasonYear": $seasonYear
     },
     "skipknowledge": true,
 	"items": (if (length != 0) then
 		(.[0].tables | map({(.group): .entries[].position})) as $groupingSeqs |
-		map(.tables[] | .group as $group | .entries[] | {
+		map(.tables[] | .group as $group | .entries[] |
+		((.club|ascii_downcase) == $favTeam) as $isFavourite | {
 			"title": "\(.position)  \(if (.tendency == "up") then "↑" elif (.tendency == "down") then "↓" else "↔" end)  \(.club)  \(if ((.club|ascii_downcase) == $favTeam) then "★" else "" end)",
 			"subtitle": "Points: \(.points)    [ GP: \(.games_played)  W: \(.wins)  L: \(.losses)  T: \(.draws)      GF: \(.goals_scored)  GA: \(.goals_against)  GD: \(.goals_difference | (if . > 0 then "+"+(.|tostring) else . end)) ]",
 			"arg": "stats",
 			"match": "\(.position) \(.club) \($group) \(.team_short_name)",
 			"icon": { "path": (if (.team_three_letter_code | IN("NYC", "MTL")) then "images/\(.team_three_letter_code).png" else "\($icons_dir)/\(.team_three_letter_code).svg" end)},
 			"text": { "copy": .club },
-			"variables": { "teamId":.team_id, "teamName":.club, "teamAbbrev":.team_three_letter_code, "points":.points, "seq":(.position + ((.subposition // 1) - 1)), "conference":$group },
+			"variables": { "favTeamNew": .club, "teamId":.team_id, "teamName":.club, "teamAbbrev":.team_three_letter_code, "points":.points, "seq":(.position + ((.subposition // 1) - 1)), "conference":$group },
 			"mods": {
 				"cmd": {"valid": false},
 				"alt": {"subtitle": "⌥↩ Sort by Conference", "arg": "", "variables": {"grouping":"conference"}},
-			    "ctrl": {"subtitle": "⌃↩ Sort by League", "arg": "", "variables": {"grouping":"league"}}
+			    "ctrl": {"subtitle": "⌃↩ Sort by League", "arg": "", "variables": {"grouping":"league"}},
+				"cmd+shift": {"subtitle": "⇧⌘↩ \(if ($isFavourite) then "Unset" else "Set" end) Favourite Team"}
 			}
 		}) | (if ($grouping != "league") then ([
 		    (unique_by(.variables.conference)[] | select((.variables.seq) == 1)) | (. |= (.variables.conference) as $conference | {
@@ -42,7 +46,7 @@ jq -cs \
 				"icon":{"path":"images/iconLarge.png"},
 				"match":"\(.variables.conference) \($groupingSeqs | map(."\($conference)" | select(.)) | join(" "))",
 				"variables":.variables, "mods":.mods, "valid": false
-			}) | (.variables.seq |= 0) | (.variables.teamName |= "")
+			}) | (.variables.seq |= 0) | (.variables.favTeamNew |= "") | (.mods."cmd+shift".subtitle |= "")
 		]+.) end)
 		| (if ($grouping == "conference") then sort_by(.variables.conference, .variables.seq) end)
 		| [(.[] | select(($grouping == "league" and .variables.seq == 1) | not) | select(.variables.seq != 0 and (.variables.teamName|ascii_downcase) == $favTeam)) | (.match |= "")] + .
